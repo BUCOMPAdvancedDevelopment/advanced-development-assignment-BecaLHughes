89fb677bb565883d4c0cbd28b0dfa832ea94affd
BecaLHughes <becalhughes@gmail.com>
2022-01-09 00:52:00 +0000

    frontend templates and user registration

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-import flask
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
-app = flask.Flask(__name__)
-@app.route("/", methods=["GET"])
-def hello():
- """ Return a friendly HTTP greeting. """
- who = flask.request.args.get("who", "World")
- return f"Hello {who}!\n"
-if __name__ == "__main__":
- # Used when running locally only. When deploying to Google App
- # Engine, a webserver process such as Gunicorn will serve the app. This
- # can be configured by adding an `entrypoint` to app.yaml.
- app.run(host="localhost", port=8080, debug=True)
+import logging
+from flask import Flask, render_template,request
+app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def home():
+ return render_template('home.html')
+@app.route('/about')
+def about():
+ return render_template('about.html')
+@app.route('/register')
+def form():
+ return render_template('register.html')
+# [END form]
+# [START submitted]
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+ name = request.form['name']
+ email = request.form['email']
+ password = request.form['password']
+ confirm_password = request.form['confirm_password']
+# form error handling
+ if password != confirm_password:
+     return render_template('register.html',
+     error="Your passwords do not match. Please try again.")
+ if len(password) < 8:
+     return render_template('register.html',
+     error="Your password needs to be at least 8 characters long." )
+ if name is None or not name or email is None:
+     return render_template('register.html',
+     error="Please fill in all boxes." )
+ # [END submitted]
+ # [START render_template]
+ return render_template(
+ 'submitted_form.html',
+ name=name,
+ email=email,
+ password=password,
+ confirm_password=confirm_password) 
+ # [END render_template]
+@app.errorhandler(500)
+def server_error(e):
+ # Log the error and stacktrace.
+ logging.exception('An error occurred during a request.')
+ return 'An internal error occurred.', 500
+@app.errorhandler(404)
+def page_not_found(error):
+ return render_template('404.html'), 404
+if __name__ == '__main__':
+ # Only run for local development.
+ app.run(host='127.0.0.1', port=8080, debug=True)
+@app.route('/productoverview')
+def productoverview():
+ return render_template('productoverview.html')
